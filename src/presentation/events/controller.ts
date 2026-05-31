57f80dcc4f3a12bef5e838770eae22436e753989
Socramazad <marcosdp242@gmail.com>
2026-05-30 20:43:25 -0500

    fix: conflictos solucionados

--- src/presentation/events/controller.ts
+++ src/presentation/events/controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-<<<<<<< HEAD
 import {
   CreateEvent,
   CreateOperationDtoFactory,
@@ -9,11 +8,8 @@ import {
   GetEvents,
   UpdateEvent,
   UpdateEventDtoFactory,
+  DeleteEventCascade,
 } from '../../domain';
-=======
-import { CreateEvent, CreateOperationDtoFactory, CustomError, DeleteEvent, DeleteEventDtoFactory, GetEvents, UpdateEvent, UpdateEventDtoFactory, DeleteEventCascade } from '../../domain';
-import { EventsRepository } from '../../domain/repositories/events.repository';
->>>>>>> d3bb79afebadec41af6210519cd61a084805b5ff
 
 export class EventsController {
   private readonly createOperationDtoFactory = new CreateOperationDtoFactory();
@@ -24,14 +20,14 @@ export class EventsController {
     private readonly getEventsUseCase: GetEvents,
     private readonly createEventUseCase: CreateEvent,
     private readonly updateEventUseCase: UpdateEvent,
-    private readonly deleteEventUseCase: DeleteEvent
+    private readonly deleteEventUseCase: DeleteEvent,
+    private readonly deleteEventCascadeUseCase: DeleteEventCascade,
   ) {}
 
   private handleError(error: unknown, res: Response) {
     if (error instanceof CustomError) {
       return res.status(error.statusCode).json({ error: error.message });
     }
-
     console.log(error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
@@ -74,7 +70,6 @@ export class EventsController {
       .catch(error => this.handleError(error, res));
   };
 
-<<<<<<< HEAD
   deleteEvent = (req: Request, res: Response) => {
     const [error, deleteEventDto] = this.deleteEventDtoFactory.create(req);
 
@@ -88,23 +83,18 @@ export class EventsController {
       .then(event => res.status(200).json(event))
       .catch(error => this.handleError(error, res));
   };
-=======
-        if (error) return res.status(400).json({ error });
-        if (!deleteEventDto) return res.status(400).json({ error: 'Invalid delete event dto' });
-        new DeleteEvent(this.eventsRepository).deleteEvent(deleteEventDto)
-            .then((event) => res.status(200).json({ event }))
-            .catch((error) => this.handleError(error, res));
+
+  deleteEventCascade = (req: Request, res: Response) => {
+    const [error, deleteEventDto] = this.deleteEventDtoFactory.create(req);
+
+    if (error) return res.status(400).json({ error });
+    if (!deleteEventDto) {
+      return res.status(400).json({ error: 'Invalid delete event dto' });
     }
 
-    deleteEventCascade = (req: Request, res: Response) => {
-        const [error, deleteEventDto] = this.deleteEventDtoFactory.create(req);
-
-        if (error) return res.status(400).json({ error });
-        if (!deleteEventDto) return res.status(400).json({ error: 'Invalid delete event dto' });
-
-        new DeleteEventCascade(this.eventsRepository).deleteEventCascade(deleteEventDto)
-            .then((result) => res.status(200).json({ events: result.events, msg: result.msg }))
-            .catch((error) => this.handleError(error, res));
-    }
->>>>>>> d3bb79afebadec41af6210519cd61a084805b5ff
+    this.deleteEventCascadeUseCase
+      .deleteEventCascade(deleteEventDto)
+      .then(result => res.status(200).json({ events: result.events, msg: result.msg }))
+      .catch(error => this.handleError(error, res));
+  };
 }
